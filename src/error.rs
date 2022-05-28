c0f0bacf03d0145f0946e8330fff098d8079f6e1
Heráclito <heraclitoqsaldanha@gmail.com>
2022-05-28 07:50:43 -0300

    add error handling

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use std::{io::{self, ErrorKind} ,fmt};
+
+#[derive(Debug, Clone)]
+pub enum NetworkPeerError{
+	Parse,
+    WouldBlock,
+    NotConnected,
+    ConnectionAborted,
+    ConnectionReset,
+    Unknown
+}
+
+impl fmt::Display for NetworkPeerError{
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<bincode::Error> for NetworkPeerError{
+	fn from(_err: bincode::Error) -> NetworkPeerError{
+		NetworkPeerError::Parse
+	}
+}
+
+impl From<io::Error> for NetworkPeerError{
+	fn from(err: io::Error) -> NetworkPeerError{
+		match err.kind(){
+            ErrorKind::WouldBlock => NetworkPeerError::WouldBlock,
+            ErrorKind::NotConnected => NetworkPeerError::NotConnected,
+            ErrorKind::ConnectionAborted => NetworkPeerError::ConnectionAborted,
+            ErrorKind::ConnectionReset => NetworkPeerError::ConnectionReset,
+            _ => NetworkPeerError::Unknown
+        }
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+mod error;
+use error::NetworkPeerError;
+
 use std::{
 	io::prelude::*,
-	error,
 	net,
 };
 
@@ -12,14 +14,14 @@ impl NetworkPeer{
 	pub fn new(stream: net::TcpStream) -> NetworkPeer{
 		NetworkPeer{stream}
 	}
-	pub fn read<T: serde::de::DeserializeOwned>(&mut self) -> Result<T, Box<dyn error::Error>>{
+	pub fn read<T: serde::de::DeserializeOwned>(&mut self) -> Result<T, NetworkPeerError>{
 		let mut size = [0u8; 2];
 		self.stream.read_exact(&mut size)?;
 		let mut data = vec![0u8; u16::from_be_bytes(size) as usize];
 		self.stream.read_exact(&mut data)?;
 		Ok(bincode::deserialize(&data)?)
 	}
-	pub fn write(&mut self, pack: &impl serde::Serialize) -> Result<(), Box<dyn error::Error>>{
+	pub fn write(&mut self, pack: &impl serde::Serialize) -> Result<(), NetworkPeerError>{
 		let data = bincode::serialize(pack)?;
 		let data = [Vec::from((data.len() as u16).to_be_bytes()), data].concat();
 		Ok(self.stream.write_all(&data)?)
